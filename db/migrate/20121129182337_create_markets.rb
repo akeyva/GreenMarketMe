661b66f394c19495a6ee1731ea8aaf7b2334e751
Akiva Leeder <akiva.leeder@gmail.com>
2012-12-05 14:20:48 -0500

    add attr to migration

--- db/migrate/20121129182337_create_markets.rb
+++ db/migrate/20121129182337_create_markets.rb
@@ -5,7 +5,9 @@ class CreateMarkets < ActiveRecord::Migration
       t.string :address
       t.string :borough
       t.string :days_open
-
+      t.float  :latitude
+      t.float  :longitude
+      t.boolean :gmaps
       t.timestamps
     end
   end
